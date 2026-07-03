b77d835a057b8544f84dc7fd44f9f6d6412735ff
ruitybalt38 <rui.amano.tfa@gmail.com>
2026-07-03 18:16:54 +0900

    アイコンを反映

--- discord/discord.go
+++ discord/discord.go
@@ -36,7 +36,8 @@ func (c *WebhookClient) SendMessage(message string, roleID string) error {
 			Parse: []string{"everyone", "users"},
 			Roles: []string{roleID},
 		},
-		Username: "もあち配信通知",
+		Username:  "もあち配信通知",
+		AvatarURL: "https://raw.githubusercontent.com/tokagezassou/youtube-live-notifier/main/assets/sample_icon.JPG",
 	}
 
 	body, err := json.Marshal(p)
